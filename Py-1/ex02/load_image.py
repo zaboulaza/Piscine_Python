005da928da1ba684e5515d071bd385000341734e
zaboulaza <smailnour94@gmail.com>
2026-05-16 18:48:50 +0200

    ex02 end

---
+++ Py-1/ex02/load_image.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+
+def ft_load(path: str) -> np.array : 
+    """charger une image et print son format"""
+    
+    try :
+        img = Image.open(path)
+    except Exception as e:
+        raise ValueError(f"Error: {e}")
+        
+    if (img.format != "JPEG") :
+        raise ValueError("Error: img dont have good format")
+
+    
+    array = np.array(img)
+    
+    print(f"The shape of image is: {array.shape}")
+    
+    return array
+    
