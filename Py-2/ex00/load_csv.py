2d8625fdcec9fa41ab8f7edbfcedfb1b4244cf97
zaboulaza <smailnour94@gmail.com>
2026-05-24 19:32:20 +0200

    ex00 end

--- Py-2/ex00/load_csv.py
+++ Py-2/ex00/load_csv.py
@@ -1,1 +1,15 @@
-def load(path: str) -> Dataset:
+import pandas as pd
+
+def load(path: str) -> pd.DataFrame:
+    """fonction qui return le csv en forme tableau"""
+    
+    try :
+        res = pd.read_csv(path)
+    except Exception as e :
+        print(f"Error: {e}")
+        return None
+    
+    print(f"Loading dataset of dimensions {res.shape}")
+    
+    return res
+
