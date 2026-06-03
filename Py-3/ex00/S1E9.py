8cf5c02cbee83c035c91b2e5b31c9d07f6e77ce3
zaboulaza <smailnour94@gmail.com>
2026-06-03 16:28:30 +0200

    ex00 end

---
+++ Py-3/ex00/S1E9.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """Your docstring for Class"""
+    @abstractmethod
+    def __init__(self, nom, is_alive=True):
+        pass
+    
+    @abstractmethod
+    def die(self) :
+        pass
+    
+class Stark(Character):
+    """Your docstring for Class"""
+    def __init__(self, nom, is_alive=True):
+        """Your docstring for Constructor"""
+        self.first_name = nom
+        self.is_alive = is_alive
+        
+    def die(self) : 
+        """Your docstring for Method"""
+        self.is_alive = False
+        
